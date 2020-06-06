15b6f390d058bbc8c0327a4ae2a6b2573de33b5c
ragnarokas <66486410+ragnarokas@users.noreply.github.com>
2020-06-06 17:27:28 +0300

    Add files via upload

---
+++ quaternions/__init__.py
@@ -0,0 +1,1 @@
+from .quaternion_calculation import quaternion_calculation
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='quaternions',
+version='0.1',
+description='Package',
+packages=['quaternions'],
+include_package_data=True,
+zip_safe=False)
